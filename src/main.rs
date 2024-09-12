37bea6800a6560dd3c98e30f5c3ed24020d3654a
coderjaisharma <jaiksinn@gmail.com>
2024-09-12 13:59:17 +0530

    fib added

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 fn main(){
    let ans=is_even(5);
    println!("{}",ans);
+   println!("{}",fib(2));
 }
 fn is_even(num:i32)->bool {
 
@@ -10,4 +11,21 @@ fn is_even(num:i32)->bool {
     }else{
         return false;
     }
+}
+fn fib(num:i32)->i32{
+    let mut first_num=0;
+    let mut second_num:i32=1;
+    if num==0{
+        return first_num;
+    }
+    if num==1{
+        return second_num;
+    }
+
+    for _ in 0..num-1{
+        let temp=second_num;
+        second_num=second_num+first_num;
+        first_num=temp;
+    }
+    return second_num;
 }
