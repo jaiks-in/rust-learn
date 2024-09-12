ddaa4dd033fc5d12a593ecff85c91c1cdc219044
coderjaisharma <jaiksinn@gmail.com>
2024-09-12 13:26:24 +0530

    rust code

---
+++ target/debug/deps/rust_project-8881d8e3e10697c6.d
@@ -0,0 +1,5 @@
+/home/jaiks/Documents/rust-project/target/debug/deps/librust_project-8881d8e3e10697c6.rmeta: src/main.rs
+
+/home/jaiks/Documents/rust-project/target/debug/deps/rust_project-8881d8e3e10697c6.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-project.d
@@ -0,0 +1,1 @@
+/home/jaiks/Documents/rust-project/target/debug/rust-project: /home/jaiks/Documents/rust-project/src/main.rs
